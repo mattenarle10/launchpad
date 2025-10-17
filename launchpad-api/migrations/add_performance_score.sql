96a23c07b5ffcc43d99facdbff5334b8f6b11090
Matthew Enarle <enarlem10@gmail.com>
2025-10-18 03:31:08 +0800

    feat: replicate db

--- launchpad-api/migrations/add_performance_score.sql
+++
@@ -1,7 +0,0 @@
--- Migration: Add performance_score column to verified_students table
--- Date: 2025-10-14
--- Description: Adds performance_score column for partner companies to assess students with qualitative ratings
-
--- Add performance_score column if it doesn't exist
-ALTER TABLE verified_students 
-ADD COLUMN IF NOT EXISTS performance_score ENUM('Excellent', 'Good', 'Satisfactory', 'Needs Improvement', 'Poor') DEFAULT NULL COMMENT 'Company performance assessment';
